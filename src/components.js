25b5089b1b3f989829aaf55eabcf10680a0361cd
Ian Whitcomb <iwhitco@us.ibm.com>
2021-08-12 16:46:11 -0500

    Loading fonts externally from AEM server.

--- src/components.js
+++ src/components.js
@@ -1,3 +1,4 @@
+import './lib/styles/fonts.css';
 import './components/header/header';
 import './components/footer/footer';
 import './components/wrapper/wrapper';
--- webpack.common.js
+++ webpack.common.js
@@ -1,17 +1,19 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlMinimizerPlugin = require("html-minimizer-webpack-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
-  entry: ['core-js/stable', './src/components.js'],
+  entry: ['core-js/stable', './src/components.js', './src/lib/styles/fonts.css'],
   mode: 'development',
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Kyndryl Web Components',
       template: 'src/public/index.html',
     }),
+    new MiniCssExtractPlugin(),
   ],
   output: {
     path: path.resolve(__dirname, 'build'),
@@ -25,6 +27,10 @@ module.exports = {
         use: {
           loader: 'babel-loader',
         }
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader", "postcss-loader"],
       },
       {
         test: /\.(png|jpg|gif|woff|woff2|eot|ttf|svg)$/i,
