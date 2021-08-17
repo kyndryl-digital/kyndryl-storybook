66c079ca56e4932d3df96e90a04950c21b0cec6f
Ian Whitcomb <iwhitco@us.ibm.com>
2021-08-17 11:42:03 -0500

    Updated docs with generic usage template.

--- src/global/settings.js
+++ src/global/settings.js
@@ -1,5 +1,5 @@
 export const settings = {
   prefix: 'kyndryl',
-  cdnUrl: 'https://publish-p35268-e142028.adobeaemcloud.com',
+  cdnUrl: 'https://publish-p35268-e142028.adobeaemcloud.com/content/dam/kyndryl-apps/navigation',
   urlPath: 'https://publish-p35268-e142028.adobeaemcloud.com',
 }
