7252dedc19cb224568b2597518ca4de60e081fc0
Ian Whitcomb <iwhitco@us.ibm.com>
2021-08-27 17:00:26 -0500

    Fixed release config.

--- release.config.js
+++ release.config.js
@@ -1,13 +1,19 @@
-{
-  "plugins": [
+module.export = {
+  plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    ["@semantic-release/github", {
-      "assets": [
-        {"path": "build/*.js", "label": "Web-component script"},
-        {"path": "build/*.css", "label": "Web-component styles"},
-        {"path": "build/*.{woff,woff2,eot,svg,ttf}", "label": "Web-component fonts"}
-      ]
-    }],
-  ]
-}
+    [
+      "@semantic-release/github",
+      {
+        assets: [
+          { path: "build/*.js", label: "Web-component script" },
+          { path: "build/*.css", label: "Web-component styles" },
+          {
+            path: "build/*.{woff,woff2,eot,svg,ttf}",
+            label: "Web-component fonts",
+          },
+        ],
+      },
+    ],
+  ],
+};
