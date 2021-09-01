b9e91104555e247e7be512f01048e93000ff7ec5
Ian Whitcomb <iwhitco@us.ibm.com>
2021-09-01 13:56:25 -0500

    fix(devops): Removed release gzip from publish workflow.

--- release.config.js
+++ release.config.js
@@ -3,16 +3,6 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    [
-      "@semantic-release/github",
-      {
-        assets: [
-          {
-            path: "kyndryl-web-components.gz",
-            label: "Kyndryl web-component bundle",
-          },
-        ],
-      },
-    ],
+    "@semantic-release/github",
   ],
 };
