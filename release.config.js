a49128cdbbbed205cbe2fafa3d3d6a84b2f6498a
Ian Whitcomb <iwhitco@us.ibm.com>
2021-09-01 12:02:06 -0500

    fix(devops): Updated semantic-release configuration.

--- release.config.js
+++ release.config.js
@@ -1,58 +1,26 @@
 module.exports = {
   branches: ["master"],
   plugins: [
-    [
-      "@semantic-release/commit-analyzer",
-      {
-        preset: "eslint",
-        releaseRules: [
-          { tag: "Breaking", release: "major" },
-          { tag: "Build", release: "minor" },
-          { tag: "Chore", release: "minor" },
-          { tag: "Fix", release: "patch" },
-          { tag: "New", release: "minor" },
-          { tag: "Update", release: "minor" },
-          { tag: "Upgrade", release: "minor" },
-        ],
-      },
-    ],
-    [
-      "@semantic-release/release-notes-generator",
-      {
-        preset: "eslint",
-        releaseRules: [
-          { tag: "Breaking", release: "major" },
-          { tag: "Build", release: "minor" },
-          { tag: "Chore", release: "minor" },
-          { tag: "Fix", release: "patch" },
-          { tag: "New", release: "minor" },
-          { tag: "Update", release: "minor" },
-          { tag: "Upgrade", release: "minor" },
-        ],
-      },
-    ],
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
     [
       "@semantic-release/changelog",
       {
         changelogFile: "CHANGELOG.md",
       },
     ],
-    "@semantic-release/github",
     [
-      "@semantic-release/git",
+      "@semantic-release/github",
       {
         assets: [
-          "package.json",
           "CHANGELOG.md",
-          { path: "build/*.js", label: "Script" },
-          { path: "build/*.css", label: "Styles" },
+          { path: "build/*.js", label: "Web-component bundle" },
+          { path: "build/*.css", label: "Global styles" },
           {
             path: "build/*.{woff,woff2,eot,svg,ttf}",
             label: "Fonts",
           },
         ],
-        message:
-          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
       },
     ],
   ],
