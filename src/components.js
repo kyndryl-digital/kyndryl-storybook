08c60879e302acc1d2aaa083f842e5a17ae42689
Dennis Dolliver <Dennis.Dolliver@kyndryl.com>
2022-01-12 12:17:00 -0600

    fix(): Reorganize css for future sass compilation. (#53)
    Co-authored-by: Dennis Dolliver <ddollive@us.ibm.com>

--- src/components.js
+++ src/components.js
@@ -1,4 +1,4 @@
-import "./lib/styles/_global.scss";
+import "./styles/_global.scss";
 import "./components/header/header";
 import "./components/footer/footer";
 import "./components/wrapper/wrapper";
