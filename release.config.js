fee5afa842c4f554ba976935dabad7f579e8415c
Ian Whitcomb <iwhitco@us.ibm.com>
2022-02-03 15:07:47 -0600

    fix(cicd): Tweaked semantic release branches.

--- release.config.js
+++ release.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   branches: [
     'master',
-    'release/+([0-9])?(.{+([0-9]),x}).x',
+    { name: 'release/1.x.x', range: '1.x', channel: '1.x' },
+    { name: 'release/2.x.x', range: '2.x', channel: '2.x' },
     { name: 'release/+([0-9])?(.{+([0-9]),x}).x-beta', prerelease: true },
     { name: 'release/+([0-9])?(.{+([0-9]),x}).x-alpha', prerelease: true }
   ],
