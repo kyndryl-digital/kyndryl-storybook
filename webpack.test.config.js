f27cb9c54704680f5e3e7528e0cecfe3ba1e072d
Ian Whitcomb <iwhitco@us.ibm.com>
2022-03-21 13:52:42 -0500

    Cleaned up unit testing and created example component tests.

---
+++ webpack.test.config.js
@@ -0,0 +1,73 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: "development",
+
+  entry: [
+    "./src/components/example/example.ts",
+  ],
+
+  output: {
+    path: path.resolve(__dirname, "test"),
+    filename: 'kyndryl-web-components.js',
+  },
+
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"]
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(),
+  ],
+
+  module: {
+    rules: [
+
+      // Processes scss files and loads then into variables for import into web components.
+      {
+        test: /\.s(c|a)ss$/,
+        include: [
+          path.resolve(__dirname, "src/components/example")
+        ],
+        use: [
+          {
+            loader: "lit-scss-loader",
+            options: {
+              minify: true,
+            },
+          },
+          "extract-loader",
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+
+      // Process typescript files using Babel to make
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "ts-loader",
+          options: {
+            transpileOnly: true
+          }
+        },
+      },
+
+      // Transform referenced assets into base64
+      {
+        test: /\.(png|jpg|gif|svg)$/i,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 10485760, // 10MB
+            },
+          },
+        ],
+      },
+
+    ]
+  },
+};
