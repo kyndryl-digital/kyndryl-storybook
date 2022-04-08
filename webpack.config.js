586a11e9739b51bc986b8ebc4aa10feb5221238e
Zak Eddington <zak.eddington@wearepop.com>
2022-04-08 13:54:05 -0700

    add eslint to storybook and build tasks

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,41 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-const TerserPlugin = require("terser-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
   devtool: 'inline-source-map',
-  mode: "development",
+  mode: 'development',
 
   entry: [
-    "core-js/stable",
-    "./src/components/components.ts",
-    "./src/styles/_global.build.scss",
+    'core-js/stable',
+    './src/components/components.ts',
+    './src/styles/_global.build.scss',
   ],
 
   output: {
-    path: path.resolve(__dirname, "build"),
+    path: path.resolve(__dirname, 'build'),
     filename: 'kyndryl-web-components.js',
   },
 
   resolve: {
-    extensions: [".ts", ".tsx", ".js"]
+    extensions: ['.ts', '.tsx', '.js']
   },
 
   plugins: [
     new HtmlWebpackPlugin({
-      title: "Kyndryl Web Components",
-      template: "src/public/index.html",
+      title: 'Kyndryl Web Components',
+      template: 'src/public/index.html',
     }),
     new MiniCssExtractPlugin({
-      filename: "kyndryl-web-components.css",
+      filename: 'kyndryl-web-components.css',
+    }),
+    new ESLintPlugin({
+      context: path.resolve(__dirname, 'src'),
+      extensions: ['.ts', '.tsx', '.js', 'jsx'],
+      exclude: 'node_modules',
     }),
   ],
 
@@ -39,39 +45,35 @@ module.exports = {
       // Process and export the global stylesheet.
       {
         test: /\.s(c|a)ss$/i,
-        exclude: [
-          path.resolve(__dirname, "src/components")
-        ],
+        exclude: path.resolve(__dirname, 'src/components'),
         use: [
           {
             loader: MiniCssExtractPlugin.loader,
             options: {
-              publicPath: (resourcePath, resourceQuery) => {
+              publicPath: () => {
                 return process.env.PUBLIC_URL || '';
               },
             },
           },
-          "css-loader",
-          "sass-loader",
+          'css-loader',
+          'sass-loader',
         ],
       },
 
       // Processes scss files and loads then into variables for import into web components.
       {
         test: /\.s(c|a)ss$/,
-        include: [
-          path.resolve(__dirname, "src/components")
-        ],
+        include: path.resolve(__dirname, 'src/components'),
         use: [
           {
-            loader: "lit-scss-loader",
+            loader: 'lit-scss-loader',
             options: {
               minify: true,
             },
           },
-          "extract-loader",
-          "css-loader",
-          "sass-loader",
+          'extract-loader',
+          'css-loader',
+          'sass-loader',
         ],
       },
 
@@ -80,7 +82,7 @@ module.exports = {
         test: /\.(ts|tsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
         },
       },
 
@@ -89,7 +91,7 @@ module.exports = {
         test: /\.(png|jpg|gif|svg)$/i,
         use: [
           {
-            loader: "url-loader",
+            loader: 'url-loader',
             options: {
               limit: 10485760, // 10MB
             },
