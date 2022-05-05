352e9e8555bbd379db3cbf2fd4dc9e8bd57f61e3
Diana C <Diana.C@ocean.ibm.com>
2022-05-05 20:13:46 +0530

    testing within react-app

--- src/components/components.ts
+++ src/components/components.ts
@@ -1,7 +1,9 @@
-import './accordion/accordion';
-import './button/button';
-import './contentBandContainer/contentBandContainer';
-import './contentCard/contentCard';
-import './modal/modal';
-import './tabs/tabs';
+// import './accordion/accordion';
+// import './button/button';
+// import './contentBandContainer/contentBandContainer';
+// import './contentCard/contentCard';
+// import './modal/modal';
+// import './tabs/tabs';
 
+export * from '../global/settings/settings';
+export * from './button/button';
