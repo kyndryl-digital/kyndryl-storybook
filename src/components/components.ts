8959a6208c09171c45c79fe0d6ad3b576fb74ced
Diana C <Diana.C@ocean.ibm.com>
2022-05-05 20:22:51 +0530

    removed export

--- src/components/components.ts
+++ src/components/components.ts
@@ -1,9 +1,7 @@
-// import './accordion/accordion';
-// import './button/button';
-// import './contentBandContainer/contentBandContainer';
-// import './contentCard/contentCard';
-// import './modal/modal';
-// import './tabs/tabs';
+import './accordion/accordion';
+import './button/button';
+import './contentBandContainer/contentBandContainer';
+import './contentCard/contentCard';
+import './modal/modal';
+import './tabs/tabs';
 
-export * from '../global/settings/settings';
-export * from './button/button';
