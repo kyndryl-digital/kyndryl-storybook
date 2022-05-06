2c933de598dd0cb8b9838be0cf48b2e2cd6d311d
Diana C <Diana.C@ocean.ibm.com>
2022-05-06 12:14:09 +0530

    adding exports to test if styles work

--- src/components/components.ts
+++ src/components/components.ts
@@ -1,8 +1,15 @@
-import '../global/settings/settings';
-import './accordion/accordion';
-import './button/button';
-import './contentBandContainer/contentBandContainer';
-import './contentCard/contentCard';
-import './modal/modal';
-import './tabs/tabs';
+// import '../global/settings/settings';
+// import './accordion/accordion';
+// import './button/button';
+// import './contentBandContainer/contentBandContainer';
+// import './contentCard/contentCard';
+// import './modal/modal';
+// import './tabs/tabs';
 
+export * from '../global/settings/settings';
+export * from './accordion/accordion';
+export * from './button/button';
+export * from './contentBandContainer/contentBandContainer';
+export * from './contentCard/contentCard';
+export * from './modal/modal';
+export * from './tabs/tabs';
