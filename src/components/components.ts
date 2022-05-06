54be9962ccfb9a1b77ade1fe7eca1ae813388bcb
Diana C <Diana.C@ocean.ibm.com>
2022-05-06 12:26:26 +0530

    revert back to import

--- src/components/components.ts
+++ src/components/components.ts
@@ -1,15 +1,6 @@
-// import '../global/settings/settings';
-// import './accordion/accordion';
-// import './button/button';
-// import './contentBandContainer/contentBandContainer';
-// import './contentCard/contentCard';
-// import './modal/modal';
-// import './tabs/tabs';
-
-export * from '../global/settings/settings';
-export * from './accordion/accordion';
-export * from './button/button';
-export * from './contentBandContainer/contentBandContainer';
-export * from './contentCard/contentCard';
-export * from './modal/modal';
-export * from './tabs/tabs';
+import './accordion/accordion';
+import './button/button';
+import './contentBandContainer/contentBandContainer';
+import './contentCard/contentCard';
+import './modal/modal';
+import './tabs/tabs';
