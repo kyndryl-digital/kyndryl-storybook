8432688005992ded9f97f526182c374400c8f434
Ian Whitcomb <iwhitco@us.ibm.com>
2022-06-10 13:34:28 -0500

    Added kyndryl.com link to header logo.

--- src/components/header/header.ts
+++ src/components/header/header.ts
@@ -43,7 +43,7 @@ export class Header extends LitElement {
     return html`
       <div class="${PREFIX_CLASS}-header__primary-nav-container">
         <div class="${PREFIX_CLASS}-header__primary-nav ${PREFIX_CLASS}-grid-container ${PREFIX_CLASS}-no-grid-gap">
-          <img class="${PREFIX_CLASS}-header__logo" .src=${logo} .alt=${'Logo'} />
+          <a href="https://kyndryl.com/"><img class="${PREFIX_CLASS}-header__logo" .src=${logo} .alt=${'Logo'} /></a>
           <slot name="primary-navigation"></slot>
           <slot name="account-navigation"></slot>
         </div>
