6921d1b1328dd804af2e2cc034a660978bd24312
diana <98527684+DianaNikita@users.noreply.github.com>
2023-08-04 18:45:02 +0530

    UI-Button component (#74)
    Co-authored-by: Diana Nikita <diananikita@Dianas-MacBook-Pro-2.local>

--- src/global/defs/themes.ts
+++ src/global/defs/themes.ts
@@ -5,4 +5,12 @@
 export enum THEMES {
   DARK_STONE = 'dark-stone',
   CLOUD = 'cloud',
+  SPRUCE_60 = 'spruce-60',
+  SPRUCE_80 = 'spruce-80',
+  DARK_GREEN_60 = 'dark-spring-green-60',
+  WARM_RED_50 = 'warm-red-50',
+  SPRING_GREEN_20 = 'spring-green-20',
+  DARK_IMAGE='dark-image',
+  DARK_STONE_80='dark-stone-80',
+  LIGHT_IMAGE='light-image'
 }
